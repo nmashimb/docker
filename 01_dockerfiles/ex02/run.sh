43c2d0e328c4434feb52137ae21638ecc0bed356
nmashimb <50702463+nmashimb@users.noreply.github.com>
2021-02-10 16:58:40 +0200

    Update run.sh

--- 01_dockerfiles/ex02/run.sh
+++ 01_dockerfiles/ex02/run.sh
@@ -1,7 +1,5 @@
+git clone https://bitbucket.org/railstutorial/sample_app_4th_ed.git main
 
 docker build -f ft-rails/Dockerfile -t ft-rails:on-build .
 docker build -t rubyrails .
 docker run -it --name ft-rails -p 3000:3000 rubyrails
-
-#takes about 12 minutes to run, just control c out of puma listening thread thing.
-#and restart the container ft-rails. Finally go to URL to localhost and port 3000.
